445f6875db87f5399022b01a040e0e01b2867cef
Keaton Hutto <keatonhutto@Keatons-MacBook-Air.local>
2022-03-14 11:57:24 -0500

    spacing

--- js/external.js
+++ js/external.js
@@ -1,13 +1,16 @@
-//alert("Welcome to my website")
-//let favColor = prompt("Whats your favorite color")
-//console.log(favColor)
-//alert("Great " + favColor + " is my favorite color also!")
-//exercise 3
-/*/!*let costOfLittle = prompt("How many days are you renting little mermaid?")
+/*alert("Welcome to my website")
+let favColor = prompt("Whats your favorite color")
+console.log(favColor)
+alert("Great " + favColor + " is my favorite color also!")*/
+
+
+/*let costOfLittle = prompt("How many days are you renting little mermaid?")
 let costOfBear = prompt("How many days are you renting Bear?")
 let costOfHerc = prompt("How many days are you renting Hercules?")
 let totalCost = ((costOfLittle * 3) + (costOfBear)* 3) + (costOfHerc * 3)*!/
-alert("The total cost is " + totalCost)*!/*/
+alert("The total cost is " + totalCost)*/
+
+
 /*let google = (400)
 let amazon = (380)
 let facebook = (350)
@@ -16,6 +19,8 @@ let googleHours = prompt("How many hours did you work at Google?")
 let amazonHours = prompt("How many hours did you work at Amazon?")
 let totalPay = (facebook * facebookHours) + (google * googleHours) + (amazon * amazonHours)
 alert("Your weekly paycheck is " + totalPay)*/
+
+
 /*let numOfClasses = prompt("How many hours of classes are you currently taking?")
 let currentSch = prompt("What days are you currently taking classes?")
 let newClass = prompt("What day is new class wanting to add?(Mon, Tues, Wed, Thru, Fri")
@@ -27,6 +32,8 @@ function enroll () {
     }
 }
 enroll()*/
+
+
 /*
 let numOfItems = prompt("How many items are you buying?")
 let dateOffer = prompt("What is the date of the offer?")
