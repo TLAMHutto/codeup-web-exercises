9d637c572bd29c113c72543f02924c52448fd922
Keaton Hutto <keatonhutto@Keatons-MacBook-Air.local>
2022-03-14 11:08:53 -0500

    exercise 3

--- js/external.js
+++ js/external.js
@@ -1,4 +1,42 @@
-alert("Welcome to my website")
-let favColor = prompt("Whats your favorite color")
-console.log(favColor)
-alert("Great " + favColor + " is my favorite color also!")
+//alert("Welcome to my website")
+//let favColor = prompt("Whats your favorite color")
+//console.log(favColor)
+//alert("Great " + favColor + " is my favorite color also!")
+//exercise 3
+/*/!*let costOfLittle = prompt("How many days are you renting little mermaid?")
+let costOfBear = prompt("How many days are you renting Bear?")
+let costOfHerc = prompt("How many days are you renting Hercules?")
+let totalCost = ((costOfLittle * 3) + (costOfBear)* 3) + (costOfHerc * 3)*!/
+alert("The total cost is " + totalCost)*!/*/
+/*let google = (400)
+let amazon = (380)
+let facebook = (350)
+let facebookHours = prompt("How many hours did you work at Facebook?")
+let googleHours = prompt("How many hours did you work at Google?")
+let amazonHours = prompt("How many hours did you work at Amazon?")
+let totalPay = (facebook * facebookHours) + (google * googleHours) + (amazon * amazonHours)
+alert("Your weekly paycheck is " + totalPay)*/
+/*let numOfClasses = prompt("How many hours of classes are you currently taking?")
+let currentSch = prompt("What days are you currently taking classes?")
+let newClass = prompt("What day is new class wanting to add?")
+function enroll () {
+    if (numOfClasses <= 12 && currentSch !== newClass) {
+        alert("You can enroll in this class")
+    } else {
+        alert("You cant take this class at this time")
+    }
+}
+enroll()*/
+/*
+let numOfItems = prompt("How many items are you buying?")
+let dateOffer = prompt("What is the date of the offer?")
+let premium = prompt("Are you a premium user?(Enter yes or no")
+let expire = ("june")
+function discount () {
+    if (numOfItems >= 2 && dateOffer === "june" || premium === "yes") {
+        alert("Enjoy your discount")
+    } else {
+        alert("Sorry no discount")
+    }
+}
+discount()*/
