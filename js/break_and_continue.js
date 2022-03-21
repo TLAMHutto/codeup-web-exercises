2f51c694db4e4645df693f5208f2895fe3368130
Keaton Hutto <keatonhutto8@gmail.com>
2022-03-21 16:25:00 -0500

    loop2

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,8 +1,14 @@
 
-var result = prompt("Enter a number between 1- 50");
-    while (result < 1 || result > 50 || isNaN(result) == true)
+var result = prompt("Enter a odd number between 1- 50");
+    while(result < 1 || result > 50 || result % 2 ===0 || isNaN(result) == true)
     {
-        var result = prompt("ENTER A NUMBER!");
-    }
-    alert("Success")
+        var result = prompt("ENTER A ODD NUMBER!");
 
+    }
+    document.write("Number to skip is " + result)
+    for (var i=1;i<=50;i++){
+        if (i%2===0 || i == result){
+            continue;
+        }
+        console.log(i)
+    }
