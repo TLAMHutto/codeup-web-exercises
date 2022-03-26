d148b7073d27575a9a1a5fe209106f72d18160cb
Keaton Hutto <keatonhutto8@gmail.com>
2022-03-25 19:38:35 -0500

    reverse

--- js/js-ii-assessment-review.js
+++ js/js-ii-assessment-review.js
@@ -74,3 +74,9 @@ function alphabetSoup (soup) {
     return sorted.join('')
 }
 console.log(alphabetSoup('jnlnvfvedfvwkm'))*/
+//Exercise 7
+/*
+function reverse (arr) {
+    return  arr.reverse()
+}
+console.log(reverse([1,2,3]))*/
