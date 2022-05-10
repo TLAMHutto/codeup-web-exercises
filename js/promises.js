cea4a1849b5ea10c0ad6b0abb79430ca4d9d9f6b
TLAMHutto <keatonhutto8@gmail.com>
2022-05-10 17:07:51 -0500

    promises completed

--- js/promises.js
+++ js/promises.js
@@ -5,7 +5,12 @@ const GitUserUrl = `https://api.github.com/users/${userName}/events/public`;
 function lastGitPush(userName) {
     fetch(`https://api.github.com/users/${userName}/events/public`, {'headers': {'Authorization': "gitApiKey"}})
     .then(response => console.log(response.json()))
-    .then(data => console.log(Promise.PromiseResult.created_at))
+    .then(data => console.log(data))
     .catch(error => console.error(error));
 }
 lastGitPush(userName)
+//function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
